5c4a179008e6340e3bb73f8fd793572cad29b008
jekhokie <jekhokie@gmail.com>
2012-08-09 19:34:37 -0400

    Update the Home Controller for PostgreSQL
    Update the Home controller to create a new user condition the
    ActiveRecord::Base way (instead of the way that MongoDB would have the
    objects to be created).

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,9 @@ class HomeController < ApplicationController
     if user_signed_in?
       # initialize the condition of the user on first login
       if current_user.condition.blank?
-        current_user.condition = Condition.new :state => 1
+        condition = Condition.create :state => 1
+
+        current_user.condition = condition
         current_user.save!
       end
 
