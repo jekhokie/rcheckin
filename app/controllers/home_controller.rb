5015f33b67aae1286791c66bcb029da5ea1dc54b
jekhokie <jekhokie@gmail.com>
2012-08-08 20:52:01 -0400

    Add Initial User Condition for Sign-On
    Initialize the User's Condition to "logged in" (1) when they initially
    sign into the application.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
   def index
-    @users = User.all
+    if user_signed_in?
+      # initialize the condition of the user on first login
+      if current_user.condition.blank?
+        current_user.condition = Condition.new :state => 1
+        current_user.save!
+      end
+
+      @users = User.all
+    end
   end
 end
