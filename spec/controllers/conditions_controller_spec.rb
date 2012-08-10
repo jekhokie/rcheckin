50e855208c884c6de5a58b76cd89714ce6712f92
jekhokie <jekhokie@gmail.com>
2012-08-10 19:47:03 -0400

    Create Conditions Controller 'update' Spec
    Create spec for Conditions controller 'update' method, to be used for
    updating the condition of the user.

--- spec/controllers/conditions_controller_spec.rb
+++ spec/controllers/conditions_controller_spec.rb
@@ -17,4 +17,28 @@ describe ConditionsController do
       assigns(:condition).should == @user.condition
     end
   end
+
+  describe "PUT 'update'" do
+    it "should find the correct the user" do
+      put :update, :user_id => @user.id
+      assigns(:user).should == @user
+    end
+
+    it "should find the correct condition" do
+      put :update, :user_id => @user.id
+      assigns(:condition).should == @user.condition
+    end
+
+    describe "for a condition's state" do
+      it "should update to reflect 'IN'" do
+        put :update, :user_id => @user.id, :condition => { :state => "true" }
+        assigns(:condition).state.should == true
+      end
+
+      it "should update to reflect 'OUT'" do
+        put :update, :user_id => @user.id, :condition => { :state => "false" }
+        assigns(:condition).state.should == false
+      end
+    end
+  end
 end
