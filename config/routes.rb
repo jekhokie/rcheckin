bd41eb73afaf2f6418de962bf93811ad32262d96
jekhokie <jekhokie@gmail.com>
2012-08-10 19:48:12 -0400

    Add Conditions 'update' Route
    Add the Conditions 'update' route (PUT) for updating a Conditions
    attributes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rcheckin::Application.routes.draw do
   authenticated :user do
     root :to => 'home#index'
     resources :users, :only => [] do
-      resource :condition, :only => [ :show ]
+      resource :condition, :only => [ :show, :update ]
     end
   end
 
