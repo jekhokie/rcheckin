16db74dffcfe009f685ebe91d0107310b4e97e8c
jekhokie <jekhokie@gmail.com>
2012-08-10 19:17:38 -0400

    Remove User Condition Init from Home Controller
    Remove the creation and initialization of a User Condition from the Home
    controller (NOT where it belongs) due to the previous two commits
    accommodating the initialization of the condition for the user, as well
    as the condition's initial state.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,6 @@
 class HomeController < ApplicationController
   def index
     if user_signed_in?
-      # initialize the condition of the user on first login
-      if current_user.condition.blank?
-        condition = Condition.create :state => 1
-
-        current_user.condition = condition
-        current_user.save!
-      end
-
       @users = User.all
     end
   end
