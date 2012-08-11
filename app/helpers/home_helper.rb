76c8ab4615f1b94d40e788ae99a5f0bb1962212e
jekhokie <jekhokie@gmail.com>
2012-08-10 22:47:58 -0400

    Add Classes and Update Styling of Buttons
    Update helper methods to add classes to buttons for QuickStat tool and
    update styling to make input buttons look nicer.

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -9,12 +9,13 @@ module HomeHelper
 
   def state_button_class(name)
     button_class  = "btn btn-large"
+    button_class += (name == "IN" ? " state-in" : " state-out")
 
     if user_signed_in?
       if name == "IN"
-        button_class += " active btn-success" if user_signed_in? &&  current_user.condition.state
+        button_class += " active btn-success" if  current_user.condition.state
       elsif name == "OUT"
-        button_class += " active btn-danger"  if user_signed_in? && !current_user.condition.state
+        button_class += " active btn-danger"  if !current_user.condition.state
       end
     else
       button_class += " btn-inverse disabled"
