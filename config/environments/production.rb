697edb479d838461d9722dc861e6e9be9f023ae2
jekhokie <jekhokie@gmail.com>
2012-08-15 18:48:40 -0400

    Enable Precompile of Assets for Foreman
    Enable the precompilation of assets for Foreman - note that you still
    need to run rake assets:precompile prior to starting Foreman.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Rcheckin::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -67,5 +67,4 @@ Rcheckin::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-
 end
