420810d2d3c7697a2133c8eb2dc2ff0437d66f64
Justin Karimi <justin.karimi@onyxpoint.com>
2012-08-24 19:35:40 -0400

    Create Application Controller User Helpers
    Create the Application controller helper methods for determining the
    current user, determining if they are signed in, etc.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
-  before_filter :prepare_for_mobile
-
   protect_from_forgery
 
+  before_filter :prepare_for_mobile
+
+  helper_method :mobile_device?
+  helper_method :current_user
+  helper_method :user_signed_in?
+
   private
+
+  # Helper methods for mobile rendering
   def mobile_device?
     if session[:mobile_param]
       session[:mobile_param] == "1"
@@ -11,7 +17,6 @@ class ApplicationController < ActionController::Base
       request.user_agent =~ /Mobile|webOS/
     end
   end
-  helper_method :mobile_device?
 
   def prepare_for_mobile
     session[:mobile_param] = params[:mobile] if params[:mobile]
@@ -20,6 +25,22 @@ class ApplicationController < ActionController::Base
       request.format = :mobile
     elsif mobile_device? and request.format.to_s == "text/javascript"
       request.format = :mobilejs
+    end
+  end
+
+  # Helper methods for OmniAuth
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+  end
+
+  def user_signed_in?
+    return 1 if current_user
+  end
+
+  def authenticate_user!
+    if !current_user
+      flash[:error] = "You need to sign in to view this page"
+      redirect_to signin_path
     end
   end
 end
