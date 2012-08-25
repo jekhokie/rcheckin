8bec026684766b5945587ea0752ae09827d7e9cc
Justin Karimi <justin.karimi@onyxpoint.com>
2012-08-24 21:18:48 -0400

    Update Authentication Controller Create Action
    Update the Authentication controller 'create' action to handle different
    scenarios of a User attempting to sign into the application.

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,8 +1,69 @@
 class AuthenticationsController < ApplicationController
+  before_filter :authenticate_user!, :except => [ :create, :new ]
+  protect_from_forgery :except => :create
+
   def new
     @authentication_providers = [ "facebook" ]
   end
 
   def create
+    auth_response = request.env['omniauth.auth']
+
+    if auth_response
+      @auth_hash = Hash.new
+
+      if params[:provider] == "facebook"
+        @auth_hash[:provider] = (auth_response['provider']      ? auth_response['provider']      : '')
+        @auth_hash[:uid]      = (auth_response['uid']           ? auth_response['uid']           : '')
+        @auth_hash[:email]    = (auth_response['info']['email'] ? auth_response['info']['email'] : '')
+      elsif params[:provider] == "google"
+        @auth_hash[:provider] = (auth_response['provider']      ? auth_response['provider']      : '')
+        @auth_hash[:uid]      = (auth_response['uid']           ? auth_response['uid']           : '')
+        @auth_hash[:email]    = (auth_response['info']['email'] ? auth_response['info']['email'] : '')
+      end
+
+      # successful authentication
+      unless @auth_hash[:provider].blank? or @auth_hash[:uid].blank?
+        @authentication = Authentication.find_by_provider_and_uid(@auth_hash[:provider], @auth_hash[:uid])
+
+        if user_signed_in?
+          if @authentication # user is already signed in and used same service to sign in again
+            flash[:notice] = "You are already signed in using #{@auth_hash[:provider].titleize}"
+          else               # user has not yet added this authentication mechanism
+            @authentication = current_user.authentications.create!(:provider => @auth_hash[:provider],
+                                                                   :uid      => @auth_hash[:uid],
+                                                                   :email    => @auth_hash[:email])
+            flash[:notice] = "Your #{@auth_hash[:provider].titleize} account has been added as a sign-in mechanism"
+          end
+
+          @users = User.all
+
+          session[:user_id]           = @authentication.user.id
+          session[:authentication_id] = @authentication.id
+
+          render "home/index"
+        else
+          if @authentication # existing user sign in
+            session[:user_id]           = @authentication.user.id
+            session[:authentication_id] = @authentication.id
+
+            @users = User.all
+
+            flash[:notice] = "Signed in successfully"
+            render "home/index"
+          else               # new user
+            session[:auth_hash] = @auth_hash
+
+            redirect_to :controller => :users, :action => :new
+          end
+        end
+      else
+        flash[:error] = "Error while authenticating via #{params[:provider].titleize} - invalid data"
+        render "new"
+      end
+    else
+      flash[:error] = "Error while authenticating with #{params[:provider].titleize} - invalid data"
+      render "new"
+    end
   end
 end
