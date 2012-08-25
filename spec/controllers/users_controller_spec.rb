5d46c061e0be07f7d804400871d24cf419a8b4ae
Justin Karimi <justin.karimi@onyxpoint.com>
2012-08-24 22:29:19 -0400

    Add User Controller Spec for 'new'
    Add User spec tests for the User controller 'new' action that is
    rendered when a new user signs into the application for the first time.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,18 +1,26 @@
 require 'spec_helper'
 
 describe UsersController do
-  before (:each) do
-    @user = FactoryGirl.create :user
-  end
-
   describe "GET 'index'" do
+    before do
+      authentication = FactoryGirl.create :authentication
+      @user = authentication.user
+      session[:user_id] = @user.id
+    end
+
     it "should return a list of users" do
-      get :index, :id => @user.id
-      assigns(:users).should_not be_nil
+      get :index
+      assigns(:users).should_not be_empty
     end
   end
 
   describe "GET 'show'" do
+    before do
+      authentication = FactoryGirl.create :authentication
+      @user = authentication.user
+      session[:user_id] = @user.id
+    end
+
     it "should return a user" do
       get :show, :id => @user.id
       assigns(:user).should_not be_nil
@@ -21,6 +29,29 @@ describe UsersController do
     it "should find the right user" do
       get :show, :id => @user.id
       assigns(:user).should == @user
+    end
+  end
+
+  describe "GET 'new'" do
+    before do
+      @authentication = FactoryGirl.build :authentication
+
+      session[:auth_hash] = { :provider => "facebook",
+                              :uid      => @authentication.uid,
+                              :email    => @authentication.email }
+      get :new
+    end
+
+    it "should return a built user" do
+      assigns(:user).should_not be_nil
+    end
+
+    it "should return the user's email assigned to provider email" do
+      assigns(:user).email.should == @authentication.email
+    end
+
+    it "should render the new (registrations) page" do
+      response.should render_template("users/new")
     end
   end
 end
